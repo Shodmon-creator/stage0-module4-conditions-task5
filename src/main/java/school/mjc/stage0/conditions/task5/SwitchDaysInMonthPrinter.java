6acbc2361e9f25a9db7937781f01e65776f9e48b
Shodmon <mamayusupovshodmon2003@gmail.com>
2022-10-31 00:52:26 +0500

    some mistakes have been corrected

--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -5,7 +5,7 @@ public class SwitchDaysInMonthPrinter {
         switch (month) {
             default -> System.out.println("wrong number!");
             case (1), (3), (5), (7), (8), (10), (12) -> System.out.println(31);
-            case (2) -> System.out.println(29);
+            case (2) -> System.out.println(28);
             case (4), (6), (9), (11) -> System.out.println(30);
         }
     }
